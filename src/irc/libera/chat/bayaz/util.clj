c445e6abe4431d2712354131f40daa327b8c5afc
jeaye <contact@jeaye.com>
2022-07-01 19:03:19 -0700

    Clean up some utils

--- src/irc/libera/chat/bayaz/util.clj
+++ src/irc/libera/chat/bayaz/util.clj
@@ -1,35 +1,31 @@
 (ns irc.libera.chat.bayaz.util
   (:require [clojure.string])
-  (:import [java.io InputStream ByteArrayOutputStream]
-           [java.text StringCharacterIterator]))
+  (:import [java.io InputStream ByteArrayOutputStream]))
 
 (def max-message-length 256)
 (defn truncate [text max-length]
-  (let [length (count text)]
-    (cond
-      (nil? text)
-      ""
-      (<= length max-length)
-      text
-      :else
-      (str (subs text 0 (dec max-length)) "…"))))
+  (cond
+    (zero? max-length)
+    ""
+
+    (<= (count text) max-length)
+    text
+
+    :else
+    (str (subs text 0 (dec max-length)) "…")))
 
 ; Ported from: https://stackoverflow.com/a/3758880
 (defn size->human-readable
-  "Converts a size, in byts, to a human readable format, such as 0 B, 1.5 kB, 10 GB, etc."
+  "Converts a size, in bytes, to a human readable format, such as 0 B, 1.5 kB, 10 GB, etc."
   [size-in-bytes]
   (if (and (< -1000 size-in-bytes) (< size-in-bytes 1000))
     (str size-in-bytes " B")
-    (let [ci (StringCharacterIterator. "kMGTPE")
-          size (reduce (fn [acc _]
-                             (if-not (or (<= acc -999950) (<= 999950 acc))
-                               (reduced acc)
-                               (do
-                                 (.next ci)
-                                 (/ acc 1000))))
-                           size-in-bytes
-                           (repeat nil))]
-      (format "%.1f %cB" (float (/ size 1000)) (.current ci)))))
+    (let [[size suffix] (loop [acc size-in-bytes
+                               suffixes "kMGTPE"]
+                          (if-not (or (<= acc -999950) (<= 999950 acc))
+                            [acc (first suffixes)]
+                            (recur (/ acc 1000) (drop 1 suffixes))))]
+      (format "%.1f %cB" (float (/ size 1000)) suffix))))
 
 (defn parse-int [int-str default]
   (try
