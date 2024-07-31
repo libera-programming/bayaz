511f95dafa4643bea06aed51532b14a1ebb8b90b
jeaye <contact@jeaye.com>
2024-07-30 20:23:58 -0700

    Bump max nick/account/hostname length

---
+++ resources/migrations/023.hostname-nick-account-size.next.sql
@@ -0,0 +1,3 @@
+alter table hostname alter column hostname type varchar(128);
+alter table nick_association alter column nick type varchar(128);
+alter table account_association alter column account type varchar(128);
