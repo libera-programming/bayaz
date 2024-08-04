6ba3c22ac6983830043d5adc64bca65731ab3dce
jeaye <contact@jeaye.com>
2024-08-04 12:28:22 -0700

    Add custom youtube title fetching

--- src/irc/libera/chat/bayaz/operation/public/core.clj
+++ src/irc/libera/chat/bayaz/operation/public/core.clj
@@ -1,5 +1,6 @@
 (ns irc.libera.chat.bayaz.operation.public.core
   (:require [clojure.string :as string]
+            [clojure.data.json :as json]
             [clj-http.client :as http]
             [clojure.core.memoize :as memo]
             [reaver]
@@ -25,6 +26,24 @@
 (defmethod process! :default
   [_op]
   :not-handled)
+
+(def http-opts {:throw-exceptions false
+                :ignore-unknown-host? true
+                :max-redirects 5
+                :redirect-strategy :graceful
+                :socket-timeout 2000
+                :connection-timeout 2000})
+
+; YouTube has blocked bayaz's IP in such a way that it always returns HTML
+; with <title>- YouTube</title>, thus leaving out all of the important bits.
+; We get around this by using oembed.
+(def youtube-domains #{"youtube.com" "m.youtube.com" "youtu.be" "youtube-nocookie.com"})
+(defn fetch-youtube-url!* [url]
+  (let [oembed-url (format "https://www.youtube.com/oembed?url=%s" url)
+        res (http/get oembed-url (assoc http-opts :accept :json))]
+    (when (= 200 (:status res))
+      (let [json-body (-> res :body json/read-str)]
+        (get json-body "title")))))
 
 (def max-content-size (* 512 1000)) ; 512 kB
 (defn fetch-url!*
@@ -52,55 +71,51 @@
    * Graceful parsing of partial and junk HTML
    * Graceful handling of junk in parsed headers"
   [url]
-  (let [http-opts {:throw-exceptions false
-                   :ignore-unknown-host? true
-                   :max-redirects 5
-                   :redirect-strategy :graceful
-                   :socket-timeout 2000
-                   :connection-timeout 2000}
-        domain (util/url->domain url)
-        ; TODO: Can we remove the HEAD altogether and just use a GET?
-        head (http/head url http-opts)
-        head-status (get head :status 404)
-        html? (some #(string/includes? (get-in head [:headers "content-type"] "") %)
-                    ["text/html" "application/xhtml+xml"])
-        redirects (:trace-redirects head)]
-    (cond
-      (not= 200 head-status)
-      nil
+  (let [domain (util/url->domain url)]
+    (if (contains? youtube-domains domain)
+      (fetch-youtube-url!* url)
+      (let [; TODO: Can we remove the HEAD altogether and just use a GET?
+            head (http/head url http-opts)
+            head-status (get head :status 404)
+            html? (some #(string/includes? (get-in head [:headers "content-type"] "") %)
+                        ["text/html" "application/xhtml+xml"])
+            redirects (:trace-redirects head)]
+        (cond
+          (not= 200 head-status)
+          nil
 
-      (not html?)
-      (let [last-domain (util/url->domain (or (last redirects) url))]
-        (merge {:status head-status
-                :content-type (get-in head [:headers "content-type"])}
-               (when-not (= domain last-domain)
-                 {:domain last-domain})
-               (let [length (util/parse-int (get-in head [:headers "content-length"]) 0)]
-                 (when-not (zero? length)
-                   {:content-length length}))))
+          (not html?)
+          (let [last-domain (util/url->domain (or (last redirects) url))]
+            (merge {:status head-status
+                    :content-type (get-in head [:headers "content-type"])}
+                   (when-not (= domain last-domain)
+                     {:domain last-domain})
+                   (let [length (util/parse-int (get-in head [:headers "content-length"]) 0)]
+                     (when-not (zero? length)
+                       {:content-length length}))))
 
-      :else
-      (try
-        (let [response (http/get url (assoc http-opts :as :stream))
-              ; TODO: We could check the status and content-type again here.
-              parsed-html (with-open [body-stream (:body response)]
-                            (-> body-stream
-                                (util/read-stream-str! max-content-size)
-                                reaver/parse))
-              title (or (-> (reaver/select parsed-html "head meta[property=og:title]")
-                            reaver/edn
-                            :attrs
-                            :content)
-                      (-> (reaver/select parsed-html "head title")
-                          reaver/text))]
-          (when (some? title)
-            {:status (:status response)
-             ; Some sites have multiple titles.
-             :title (if (seq? title)
-                      (first title)
-                      title)}))
-        (catch Exception _
-          nil)))))
+          :else
+          (try
+            (let [response (http/get url (assoc http-opts :as :stream))
+                  ; TODO: We could check the status and content-type again here.
+                  parsed-html (with-open [body-stream (:body response)]
+                                (-> body-stream
+                                    (util/read-stream-str! max-content-size)
+                                    reaver/parse))
+                  title (or (-> (reaver/select parsed-html "head meta[property=og:title]")
+                                reaver/edn
+                                :attrs
+                                :content)
+                            (-> (reaver/select parsed-html "head title")
+                                reaver/text))]
+              (when (some? title)
+                {:status (:status response)
+                 ; Some sites have multiple titles.
+                 :title (if (seq? title)
+                          (first title)
+                          title)}))
+            (catch Exception _
+              nil)))))))
 (def fetch-url! (memo/ttl fetch-url!* {} :ttl/threshold (* 60 1000))); ms
 
 (defn url-info->message [info]
